5fb03401ab17bfc49b8a414944be4886a053d4a7
Michael Byers <mikebyers24@gmail.com>
2024-07-30 09:55:05 -0400

    created csv file for primary fur color counts of squirrels.

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import pandas
 
+data = pandas.read_csv('./squirrel_data.csv')
+cinnamon = len(data[data['Primary Fur Color'] == 'Cinnamon'])
+gray = len(data[data['Primary Fur Color'] == 'Gray'])
+black = len(data[data['Primary Fur Color'] == 'Black'])
+
+
+squirrel_dict = {
+    "Fur Color": ['Cinnamon', 'Gray', 'Black'],
+    "Count": [cinnamon, gray, black]
+}
+
+pandas.DataFrame(squirrel_dict).to_csv('squirrel_count.csv')
+
+
